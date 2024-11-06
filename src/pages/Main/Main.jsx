a80b86fc7c0822987a7bfa834341d7f5adfa1909
Viktor Konovalov <viseko23@gmail.com>
2024-11-06 18:42:59 +0700

    Test tailwind block

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,6 +5,10 @@ const Main = () => {
     <div
       className={styles.main}
     >
+
+      <div className="bg-green-500 text-white px-10 py-5 rounded-lg">
+        <h1 className="text-2xl font-bold">Ну здарова ёпта</h1>
+      </div>
     </div>
   );
 };
